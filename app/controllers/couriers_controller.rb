f024499756e7380bf34ae9418df493d575f199c9
Denis Golovin <ayreon25@gmail.com>
2021-07-11 00:17:28 +0300

    Courier CRUD

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -2,4 +2,44 @@ class CouriersController < ApplicationController
     def index
         @couriers = Courier.all
     end
+
+    def new
+        @courier = Courier.new
+    end
+
+    def create
+        @courier = Courier.new courier_params
+        if @courier.save
+            redirect_to couriers_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @courier = Courier.find_by id: params[:id]
+    end
+
+    def update
+        @courier = Courier.find_by id: params[:id]
+        if @courier.update courier_params
+            redirect_to couriers_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @courier = Courier.find_by id: params[:id]
+        @courier.destroy
+        redirect_to couriers_path
+    end
+
+    def show
+        @courier = Courier.find_by id: params[:id]        
+    end
+
+    def courier_params
+        params.require(:courier).permit(:name, :email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/couriers', to: 'couriers#index'
+  resources :couriers
 
-  root 'pages#index';
+  root 'pages#index'
 end
